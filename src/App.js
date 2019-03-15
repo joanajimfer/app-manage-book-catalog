015516def04ac6770a8a90afb06814c049b77c26
joanajimfer <joanajimfer@gmail.com>
2019-03-15 13:00:49 +0100

    Add styles to the item list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import Logo from './images/logo.png';
+import BookIMage from './images/bookImage.png';
 import './App.css';
 
 const booksArray = []
   
-  class App extends React.Component {
+  class App extends Component {
   
     constructor() {
       super();
@@ -55,12 +56,12 @@ const booksArray = []
 
     getDataFromLocalStorage() {
       const savedBooks = localStorage.getItem("booksInList");
-      if (savedBooks.length == 0) {
+      if (!savedBooks) {
         return [];
       } else {
         return JSON.parse(savedBooks);
       }
-    }
+    } 
 
   
     render() {
@@ -69,11 +70,14 @@ const booksArray = []
       this.saveInLocalStorage();
       return (   
         <div>
-          <h2>Add a new book:</h2>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" ref="name" placeholder="name" required/>
-            <input type="number" ref="price" placeholder="price" />
-            <select type="text" ref="gender" placeholder='gender'>
+          <header className='header__main'>
+          <img className='header__logo'src={Logo}/>
+          <section className='bookSearch__section'>
+          <h2 className='header__title'>Save a new book:</h2>
+          <form onSubmit={this.handleSubmit} className='header__form'>
+            <input type="text" ref="name" placeholder="name" required className='form__input--text'/>
+            <input type="number" ref="price" placeholder="price" className='form__input--text' />
+            <select type="text" ref="gender" placeholder='gender' className='form__input--select'>
               <option value='Terror'>Terror</option>
               <option value='Romance'>Romance</option>
               <option value='Adventures'>Adventures</option>
@@ -81,19 +85,24 @@ const booksArray = []
               <option value='Informative'>Informative</option>
               <option value='Other'>Other</option>
             </select>
-            <button type="submit">Submit</button>
+            <button type="submit" className='form__btn'>Submit</button>
           </form>
-          <h2>Book Catalogue:</h2>
-          <ul>
+          </section>
+          </header>
+          <main>
+          <h2 className='catalogue__title'>Book Catalogue:</h2>
+          <ul className='catalogue__list'>
             {books.map((book) => 
-             <li>
-               <h3>NAME: {book.name}</h3>
-               <p>PRICE: {book.price}</p>
-               <h4>GENDER: {book.gender}</h4>
+             <li className='catalogue__list--item'>
+               <h3 className='catalogue__list--title'>{book.name}</h3>
+               <img className='catalogue__list--img' src={BookIMage} alt='Cover book example image' /> 
+               <p className='catalogue__list--price' >PRICE: {book.price}€</p>
+               <h4 className='catalogue__list--gender' >{book.gender}</h4>
                <button onClick={this.handleDelete.bind(this, book)}>Delete</button>
              </li>
             )}
           </ul>
+          </main>
         </div>
       ) 
     }
